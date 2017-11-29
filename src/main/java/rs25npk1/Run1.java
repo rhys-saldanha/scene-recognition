308ce2add2aed965ad148214c68e3eb1c0507662
npk1g15 <npk1g15@soton.ac.uk>
2017-11-29 21:42:42 +0000

    [KNN] Implement TinyImageFeatureExtractor

---
+++ src/main/java/rs25npk1/Run1.java
@@ -0,0 +1,81 @@
+package rs25npk1;
+
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.commons.vfs2.FileSystemException;
+import org.openimaj.data.dataset.GroupedDataset;
+import org.openimaj.data.dataset.VFSGroupDataset;
+import org.openimaj.data.dataset.VFSListDataset;
+import org.openimaj.feature.DoubleFV;
+import org.openimaj.feature.FeatureExtractor;
+import org.openimaj.image.FImage;
+import org.openimaj.image.ImageUtilities;
+import org.openimaj.image.processing.resize.ResizeProcessor;
+import org.openimaj.util.array.ArrayUtils;
+
+public class Run1 {
+	
+	private static final int K = 15;
+	private static final int SQUARE_SIZE = 16;
+
+    public static void main(String[] args) throws FileSystemException, URISyntaxException {
+    	URL testing = ClassLoader.getSystemResource("res/testing.zip");
+    	URL training = ClassLoader.getSystemResource("res/training.zip");
+    	
+    	GroupedDataset<String, VFSListDataset<FImage>, FImage> trainingData = new VFSGroupDataset<FImage>(training.toURI().getPath(), ImageUtilities.FIMAGE_READER);
+    	VFSListDataset<FImage> testData = new VFSListDataset<FImage>(testing.toURI().getPath(), ImageUtilities.FIMAGE_READER);
+    	
+    	
+    	
+    	
+    }
+    
+    static class TinyImageFeatureExtractor implements FeatureExtractor<DoubleFV, FImage> {
+
+		@Override
+		public DoubleFV extractFeature(FImage image) {
+			// Find the smallest dimension to make a square
+			int size = Math.min(image.height, image.width);
+			// Extract square from centre of image
+			image = image.extractCenter(size, size);
+			// Resize
+			image.processInplace(new ResizeProcessor(SQUARE_SIZE, SQUARE_SIZE));
+			// Return vector from 2D array of pixel values
+			DoubleFV feature = new DoubleFV(ArrayUtils.reshape(ArrayUtils.convertToDouble(image.pixels)));
+			// Zero mean, unit length and return
+			return unitLength(zeroMean(feature));
+		}
+		
+		private DoubleFV zeroMean(DoubleFV feature) {
+			feature = feature.clone();
+			double mean = 0;
+			for (double d : feature.values) {
+				mean += d;
+			}
+			mean /= feature.values.length;
+			double[] newValues = new double[feature.values.length];
+			for (int i = 0; i < feature.values.length; i++) {
+				newValues[i] = feature.values[i] - mean;
+			}
+			return new DoubleFV(newValues);
+		}
+		
+		private DoubleFV unitLength(DoubleFV feature) {
+			feature = feature.clone();
+			double sum = 0;
+			for (double d : feature.values) {
+				sum += d;
+			}
+			double[] newValues = new double[feature.values.length];
+			for (int i = 0; i < feature.values.length; i++) {
+				newValues[i] = feature.values[i] / sum;
+			}
+			return new DoubleFV(newValues);
+		}
+    	
+    }
+}

53e8379162ab4a4656d27f9c63dc4275742f150b
RHYS SALDANHA <rhys.saldanha@gmail.com>
2017-12-06 20:31:09 +0000

    [KNN] Split training data to test algorithm

--- src/main/java/rs25npk1/Run1.java
+++ src/main/java/rs25npk1/Run1.java
@@ -2,11 +2,12 @@ package rs25npk1;
 
 import org.apache.commons.vfs2.FileSystemException;
 import org.openimaj.data.dataset.GroupedDataset;
-import org.openimaj.data.dataset.ListDataset;
 import org.openimaj.data.dataset.VFSGroupDataset;
 import org.openimaj.data.dataset.VFSListDataset;
+import org.openimaj.experiment.dataset.split.GroupedRandomSplitter;
 import org.openimaj.feature.DoubleFV;
 import org.openimaj.feature.FeatureExtractor;
+import org.openimaj.image.DisplayUtilities;
 import org.openimaj.image.FImage;
 import org.openimaj.image.ImageUtilities;
 import org.openimaj.image.processing.resize.ResizeProcessor;
@@ -15,41 +16,71 @@ import org.openimaj.util.array.ArrayUtils;
 import org.openimaj.util.pair.IntDoublePair;
 
 import java.net.URISyntaxException;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Run1 {
 
     private static final int K = 15;
     private static final int SQUARE_SIZE = 16;
+    private static String[] classes;
+    private static DoubleNearestNeighboursExact knn;
+    private static FeatureExtractor<DoubleFV, FImage> featureExtractor;
 
     public static void main(String[] args) throws FileSystemException, URISyntaxException {
         // Get training and testing data
         GroupedDataset<String, VFSListDataset<FImage>, FImage> trainingData = new VFSGroupDataset<>("zip:http://comp3204.ecs.soton.ac.uk/cw/training.zip", ImageUtilities.FIMAGE_READER);
-        ListDataset<FImage> testingData = new VFSListDataset<>("zip:http://comp3204.ecs.soton.ac.uk/cw/testing.zip", ImageUtilities.FIMAGE_READER);
+        // Remove training folder
+        trainingData.remove("training");
+//        ListDataset<FImage> testingData = new VFSListDataset<>("zip:http://comp3204.ecs.soton.ac.uk/cw/testing.zip", ImageUtilities.FIMAGE_READER);
+
+        GroupedRandomSplitter<String, FImage> splitter = new GroupedRandomSplitter<>(trainingData, 80,0,20);
+
 
         // Instance of our feature extractor
-        FeatureExtractor<DoubleFV, FImage> featureExtractor = new TinyImageFeatureExtractor();
+        featureExtractor = new TinyImageFeatureExtractor();
 
         // Array of feature vectors
-        double[][] features = new double[trainingData.numInstances()][];
+        double[][] features = new double[splitter.getTrainingDataset().numInstances()][];
         // Array of classes
-        String[] classes = new String[trainingData.numInstances()];
+        classes = new String[splitter.getTrainingDataset().numInstances()];
 
         AtomicInteger i = new AtomicInteger(0);
-        trainingData.forEach((className, imageList) -> {
+        splitter.getTrainingDataset().forEach((className, imageList) -> {
             imageList.forEach(image -> {
                 features[i.get()] = featureExtractor.extractFeature(image).values;
                 classes[i.getAndIncrement()] = className;
             });
         });
 
-        DoubleNearestNeighboursExact knn = new DoubleNearestNeighboursExact(features);
+        knn = new DoubleNearestNeighboursExact(features);
 
-        List<IntDoublePair> result = knn.searchKNN(featureExtractor.extractFeature(trainingData.getInstances("store").getInstance(0)).values, K);
+        AtomicInteger correct = new AtomicInteger();
+        splitter.getTestDataset().forEach((c, v) -> {
+            v.forEach(image ->{
+                String s = classify(image);
+                if (c.equals(s)) {
+                    correct.getAndIncrement();
+                } else {
+                    DisplayUtilities.display(image, s);
+                }
+            });
+        });
+        System.out.println((float) correct.get() / splitter.getTestDataset().numInstances());
+    }
 
-        result.forEach(p -> System.out.println(p.toString()));
-        System.out.println(result.size());
+    private static String classify(FImage randomImage) {
+        List<IntDoublePair> nn = knn.searchKNN(featureExtractor.extractFeature(randomImage).values, K);
+        Map<String, Integer> results = new HashMap<>();
+
+//        nn.forEach(p -> System.out.println(p.toString()));
+        nn.forEach(p -> {
+            String c = classes[p.getFirst()];
+            int value = results.get(c) == null ? 0 : results.get(c);
+            results.put(c, value + 1);
+        });
+//        results.forEach((k, v) -> System.out.println(String.format("%s, %s", k, v)));
+        return Collections.max(results.entrySet(), Map.Entry.comparingByValue()).getKey();
     }
 
     static class TinyImageFeatureExtractor implements FeatureExtractor<DoubleFV, FImage> {

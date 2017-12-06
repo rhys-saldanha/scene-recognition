1861abd53ee363a68d8a325d81dc7d3e600afe6e
RHYS SALDANHA <rhys.saldanha@gmail.com>
2017-12-06 18:13:41 +0000

    [KNN] Reimplement unitLength, find nearest neighbours

--- src/main/java/rs25npk1/Run1.java
+++ src/main/java/rs25npk1/Run1.java
@@ -2,21 +2,21 @@ package rs25npk1;
 
 import org.apache.commons.vfs2.FileSystemException;
 import org.openimaj.data.dataset.GroupedDataset;
+import org.openimaj.data.dataset.ListDataset;
 import org.openimaj.data.dataset.VFSGroupDataset;
 import org.openimaj.data.dataset.VFSListDataset;
 import org.openimaj.feature.DoubleFV;
 import org.openimaj.feature.FeatureExtractor;
-import org.openimaj.image.DisplayUtilities;
 import org.openimaj.image.FImage;
 import org.openimaj.image.ImageUtilities;
 import org.openimaj.image.processing.resize.ResizeProcessor;
+import org.openimaj.knn.DoubleNearestNeighboursExact;
 import org.openimaj.util.array.ArrayUtils;
+import org.openimaj.util.pair.IntDoublePair;
 
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Run1 {
 
@@ -26,19 +26,30 @@ public class Run1 {
     public static void main(String[] args) throws FileSystemException, URISyntaxException {
         // Get training and testing data
         GroupedDataset<String, VFSListDataset<FImage>, FImage> trainingData = new VFSGroupDataset<>("zip:http://comp3204.ecs.soton.ac.uk/cw/training.zip", ImageUtilities.FIMAGE_READER);
-        VFSListDataset<FImage> testingData = new VFSListDataset<>("zip:http://comp3204.ecs.soton.ac.uk/cw/testing.zip", ImageUtilities.FIMAGE_READER);
+        ListDataset<FImage> testingData = new VFSListDataset<>("zip:http://comp3204.ecs.soton.ac.uk/cw/testing.zip", ImageUtilities.FIMAGE_READER);
 
         // Instance of our feature extractor
         FeatureExtractor<DoubleFV, FImage> featureExtractor = new TinyImageFeatureExtractor();
-        // Map of group to list of feature vectors
-        Map<String, List<DoubleFV>> features = new HashMap<>();
-        // Initialise an empty list for each group
-        trainingData.getGroups().forEach(s -> features.put(s, new ArrayList<>()));
-        // For each image in each group, find the feature vector and
-        //  add to the appropriate list
-        trainingData.forEach((key, value) ->
-                value.forEach(i ->
-                        features.get(key).add(featureExtractor.extractFeature(i))));
+
+        // Array of feature vectors
+        double[][] features = new double[trainingData.numInstances()][];
+        // Array of classes
+        String[] classes = new String[trainingData.numInstances()];
+
+        AtomicInteger i = new AtomicInteger(0);
+        trainingData.forEach((className, imageList) -> {
+            imageList.forEach(image -> {
+                features[i.get()] = featureExtractor.extractFeature(image).values;
+                classes[i.getAndIncrement()] = className;
+            });
+        });
+
+        DoubleNearestNeighboursExact knn = new DoubleNearestNeighboursExact(features);
+
+        List<IntDoublePair> result = knn.searchKNN(featureExtractor.extractFeature(trainingData.getInstances("store").getInstance(0)).values, K);
+
+        result.forEach(p -> System.out.println(p.toString()));
+        System.out.println(result.size());
     }
 
     static class TinyImageFeatureExtractor implements FeatureExtractor<DoubleFV, FImage> {
@@ -54,7 +65,7 @@ public class Run1 {
             // Return vector from 2D array of pixel values
             DoubleFV feature = new DoubleFV(ArrayUtils.reshape(ArrayUtils.convertToDouble(image.pixels)));
             // Zero mean, unit length and return
-            return (zeroMean(feature)).normaliseFV();
+            return unitLength(zeroMean(feature));
         }
 
         private DoubleFV zeroMean(DoubleFV feature) {
@@ -67,6 +78,20 @@ public class Run1 {
             double[] newValues = new double[feature.values.length];
             for (int i = 0; i < feature.values.length; i++) {
                 newValues[i] = feature.values[i] - mean;
+            }
+            return new DoubleFV(newValues);
+        }
+
+        private DoubleFV unitLength(DoubleFV feature) {
+            feature = feature.clone();
+            double sum = 0;
+            for (double d : feature.values) {
+                sum += Math.pow(d, 2);
+            }
+            sum = Math.sqrt(sum);
+            double[] newValues = new double[feature.values.length];
+            for (int i = 0; i < feature.values.length; i++) {
+                newValues[i] = feature.values[i] / sum;
             }
             return new DoubleFV(newValues);
         }

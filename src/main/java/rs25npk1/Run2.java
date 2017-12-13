117b53e0e14d3ec0dbb205eea46ae36740cf1695
npk1g15 <npk1g15@soton.ac.uk>
2017-12-13 17:44:24 +0000

    [LIN] Implement LocalFeatureExtractor

--- src/main/java/rs25npk1/Run2.java
+++ src/main/java/rs25npk1/Run2.java
@@ -1,5 +1,18 @@
 package rs25npk1;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openimaj.feature.DoubleFV;
+import org.openimaj.feature.local.LocalFeature;
+import org.openimaj.feature.local.LocalFeatureExtractor;
+import org.openimaj.feature.local.LocalFeatureImpl;
+import org.openimaj.feature.local.SpatialLocation;
+import org.openimaj.image.FImage;
+import org.openimaj.image.pixel.sampling.RectangleSampler;
+import org.openimaj.math.geometry.shape.Rectangle;
+import org.openimaj.util.array.ArrayUtils;
+
 public class Run2 extends Main{
 
 	@Override
@@ -16,5 +29,39 @@ public class Run2 extends Main{
 	 * 
 	 */
     
+	private int PATCH_SIZE = 8;
+	private int PATCH_STEP = 4;
+	
+	public class PatchesExtractor implements LocalFeatureExtractor<LocalFeature<SpatialLocation, DoubleFV>, FImage> {
+
+		@Override
+		public List<? extends LocalFeature<SpatialLocation, DoubleFV>> extractFeature(FImage image) {
+		    List<LocalFeature<SpatialLocation, DoubleFV>> allPatches = new ArrayList<>();
+			
+			RectangleSampler patches = new RectangleSampler(image, PATCH_STEP, PATCH_STEP, PATCH_SIZE, PATCH_SIZE);
+			
+	        for(Rectangle patch : patches){
+	            FImage area = image.extractROI(patch);
+
+	            //2D array to 1D array
+	            double[] vector = ArrayUtils.reshape(ArrayUtils.convertToDouble(area.pixels));
+	            DoubleFV featureV = new DoubleFV(vector);
+	            //Location of rectangle is location of feature
+	            SpatialLocation sl = new SpatialLocation(patch.x, patch.y);
+	            
+	            //Generate as a local feature for compatibility with other modules
+	            LocalFeature<SpatialLocation, DoubleFV> lf = new LocalFeatureImpl<SpatialLocation, DoubleFV>(sl,featureV);
+
+	            allPatches.add(lf);
+	        }
+			return allPatches;
+		}
+
+		@Override
+		public Class<? extends LocalFeature<?, ?>> getFeatureClass() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+	}
 	
 }

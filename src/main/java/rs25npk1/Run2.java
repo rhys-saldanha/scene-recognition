46f3537407ac45958c51bf54d2c8c8f54808df15
npk1g15 <npk1g15@soton.ac.uk>
2017-12-13 17:11:33 +0000

    [LIN] Add steps to do

--- src/main/java/rs25npk1/Run2.java
+++ src/main/java/rs25npk1/Run2.java
@@ -1,4 +1,20 @@
 package rs25npk1;
 
 public class Run2 extends Main{
+
+	@Override
+	void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	/*
+	 * 
+	 * Step 1 : Create HardAssigner 
+	 * Step 2 : Give HardAssigner to extractor to get patches
+	 * Step 3 : Extractor to LiblinearAnnotator
+	 * Step 4 : Train + Test
+	 * 
+	 */
+    
+	
 }

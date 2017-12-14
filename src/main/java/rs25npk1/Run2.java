0d8d8e4d44a5fd7e71c9469695134fe1756a8676
RHYS SALDANHA <rhys.saldanha@gmail.com>
2017-12-14 00:12:24 +0000

    [LIN] Comment PatchesExtractor

--- src/main/java/rs25npk1/Run2.java
+++ src/main/java/rs25npk1/Run2.java
@@ -46,7 +46,7 @@ public class Run2 extends Main {
 
     private int PATCH_SIZE = 8;
     private int PATCH_STEP = 4;
-    
+
     public static void main(String[] args) {
         (new Run2()).run();
     }
@@ -61,22 +61,22 @@ public class Run2 extends Main {
         featureExtractor = new PatchFeatureExtractor(assigner);
 
         ann = new LiblinearAnnotator<FImage, String>(featureExtractor, Mode.MULTICLASS, SolverType.L2R_L2LOSS_SVC, 1.0, 0.00001);
-        
+
         ann.train(trainingData);
 
         //TESTING WITH SMALL TEST SET
         testingData = (VFSListDataset<FImage>) testingData.subList(0, 100);
-        
+
         System.err.println("Testing");
         Map<String, String> results = new HashMap<>();
         for (int i = 0; i < testingData.size(); i++) {
             FImage image = testingData.get(i);
             String name = testingData.getID(i);
             String guess = ann.classify(image).toString();
-            
+
             results.put(name, guess);
         }
-        
+
         try (Writer writer = new FileWriter(new File("run2.txt"))) {
             System.err.println("Classified");
             IntStream.range(0, testingData.size()).forEach(index -> {
@@ -89,13 +89,13 @@ public class Run2 extends Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
+
         System.err.println("Fin!");
     }
 
     public HardAssigner<double[], double[], IntDoublePair> makeAssigner(GroupedDataset<String, ListDataset<FImage>, FImage> groupedDataset) {
         List<LocalFeature<SpatialLocation, DoubleFV>> patches = new ArrayList<>();
-        
+
         LocalPatchesExtractor extractor = new LocalPatchesExtractor();
         for (FImage i : groupedDataset) {
             patches.addAll(extractor.extractFeature(i));
@@ -105,39 +105,53 @@ public class Run2 extends Main {
         for (LocalFeature p : patches) {
             keys.add(p.getFeatureVector().asDoubleVector());
         }
-        
+
         // (try ~500 clusters to start)
         DoubleKMeans dkm = DoubleKMeans.createKDTreeEnsemble(500);
-        
+
         System.err.println(".cluster() is what's slow (more mem maybe?)");
-        
+
         DoubleCentroidsResult clusters = dkm.cluster(keys.toArray(new double[][]{}));
-        
+
         System.err.println(".cluster() done. Ayyyyyy");
-        
+
         return clusters.defaultHardAssigner();
     }
 
-
     class LocalPatchesExtractor implements LocalFeatureExtractor<LocalFeature<SpatialLocation, DoubleFV>, FImage> {
+        private int PATCH_SIZE, STEP;
 
+        /**
+         * Construct a local feature extractor with a patch size and step size. If the step size
+         * is less than the patch size, the patches will overlap.
+         *
+         * @param patch size in pixels of patch, patches are square
+         * @param step  length in pixels between the start of the patches
+         */
+        LocalPatchesExtractor(int patch, int step) {
+            this.PATCH_SIZE = patch;
+            this.STEP = step;
+        }
+
+        /**
+         * Extracts a list of local features from an image, based on the patch size and patch step
+         *
+         * @param image extract local features from this
+         * @return list of extracted local features
+         */
         @Override
         public List<LocalFeature<SpatialLocation, DoubleFV>> extractFeature(FImage image) {
             List<LocalFeature<SpatialLocation, DoubleFV>> allPatches = new ArrayList<>();
 
-            RectangleSampler patches = new RectangleSampler(image, PATCH_STEP, PATCH_STEP, PATCH_SIZE, PATCH_SIZE);
-
-            for (Rectangle patch : patches) {
-                FImage area = image.extractROI(patch);
-
-                //2D array to 1D array
-                double[] vector = ArrayUtils.reshape(ArrayUtils.convertToDouble(area.pixels));
-                DoubleFV featureVector = new DoubleFV(vector);
-                //Location of rectangle is location of feature
+            // Use OpenImaj RectangleSampler to generate patches
+            // Cannot use subImageIterator as we need patch locations
+            for (Rectangle patch : new RectangleSampler(image, STEP, STEP, PATCH_SIZE, PATCH_SIZE)) {
+                // Feature vector is defined as the values of the patch pixels
+                // Patches are constant size therefore feature vectors have constant dimensionality
+                DoubleFV featureVector = new DoubleFV(image.extractROI(patch).getDoublePixelVector());
+                // Location of feature is the location of the patch
                 SpatialLocation location = new SpatialLocation(patch.x, patch.y);
-
-                //Generate as a local feature for compatibility with other modules
-                LocalFeature<SpatialLocation, DoubleFV> localFeature = new LocalFeatureImpl<SpatialLocation, DoubleFV>(location, featureVector);
+                LocalFeature<SpatialLocation, DoubleFV> localFeature = new LocalFeatureImpl<>(location, featureVector);
 
                 allPatches.add(localFeature);
             }
@@ -146,7 +160,7 @@ public class Run2 extends Main {
 
         @Override
         public Class<LocalFeature<SpatialLocation, DoubleFV>> getFeatureClass() {
-            // TODO Auto-generated method stub
+            // Not needed
             return null;
         }
     }
@@ -164,7 +178,7 @@ public class Run2 extends Main {
             BagOfVisualWords<double[]> bag = new BagOfVisualWords<double[]>(assigner);
 
             BlockSpatialAggregator<double[], SparseIntFV> spatial = new BlockSpatialAggregator<double[], SparseIntFV>(bag, 2, 2);
-                    
+
             return spatial.aggregate(localExtractor.extractFeature(image), image.getBounds()).normaliseFV();
         }
 
